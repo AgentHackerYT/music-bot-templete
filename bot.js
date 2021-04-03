4e61f3d4cd91f8c4c2dd1fb303c512cf59a46248
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-04-03 10:33:14 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzg1MDY3MDgzNTY2MDIyNjc2.X8ycsg.GSBjHN3Ulse6EQpdJnIb4SLqRSE',
+        prefix: '--',
+        activity: 'ACTIVITY',
+      },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
---
+++ trackStart.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js')
+module.exports = (client, message, track) => {
+    message.channel.send(`${client.emotes.music} - Now playing ${track.title} into ${message.member.voice.channel.name} ...`);
+};
